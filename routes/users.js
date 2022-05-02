bb9f22a8c87163e2b27aa0f001d73a85140808ed
abiringira <abiringiraemmanuel@gmail.com>
2022-05-02 20:00:58 +0200

    Creating generate report function

--- routes/users.js
+++ routes/users.js
@@ -905,6 +905,33 @@ app.get('/logout',function(req,res) {
 
 })
 
+app.get("/generateReport", (req, res) => {
+    ejs.renderFile(path.join(__dirname, './views/requests/', "report-      project.ejs"), {students: students}, (err, data) => {
+    if (err) {
+          res.send(err);
+    } else {
+        let options = {
+            "height": "11.25in",
+            "width": "8.5in",
+            "header": {
+                "height": "20mm"
+            },
+            "footer": {
+                "height": "20mm",
+            },
+        };
+        pdf.create(data, options).toFile("report.pdf", function (err, data) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("File created successfully");
+            }
+        });
+    }
+})
+}
+)
+
 
 
 
